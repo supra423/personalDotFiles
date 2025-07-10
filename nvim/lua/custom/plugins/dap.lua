96c31f6dae8668c4ae1cd7e49bef5dc05863f263
supra <alexusenzoroa@gmail.com>
2025-07-10 10:18:49 +0800

    Another update!

--- nvim/lua/custom/plugins/dap.lua
+++ nvim/lua/custom/plugins/dap.lua
@@ -18,12 +18,21 @@ return {
 			dap.adapters.cppdbg = {
 				id = "cppdbg",
 				type = "executable",
-				command = "/home/supra/.local/share/cpptools/extension/debugAdapters/bin/OpenDebugAD7",
+				command = "/home/supra/.local/share/nvim/mason/bin/OpenDebugAD7",
+			}
+			dap.adapters.gdb = {
+				type = "executable",
+				command = "gdb",
+				args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+			}
+			dap.adapters.codelldb = {
+				type = "executable",
+				command = "codelldb",
 			}
 			dap.configurations.rust = {
 				{
 					name = "Launch file",
-					type = "cppdbg",
+					type = "codelldb",
 					request = "launch",
 					program = function()
 						return vim.fn.input(
@@ -33,14 +42,52 @@ return {
 						)
 					end,
 					cwd = "${workspaceFolder}",
-					stopAtEntry = false,
+					-- stopAtEntry = false,
+					stopAtBeginningOfMainSubprogram = false,
+					args = {},
+					initCommands = function()
+						local rustc_sysroot = vim.fn.trim(vim.fn.system("rustc --print sysroot"))
+						local script_import = 'command script import "'
+							.. rustc_sysroot
+							.. '/lib/rustlib/etc/lldb_lookup.py"'
+						local commands_file = rustc_sysroot .. "/lib/rustlib/etc/lldb_commands"
+
+						local commands = {}
+						local file = io.open(commands_file, "r")
+						if file then
+							for line in file:lines() do
+								table.insert(commands, line)
+							end
+							file:close()
+						end
+						table.insert(commands, 1, script_import)
+						return commands
+					end,
 				},
+				-- {
+				-- 	name = "Select and attach to process",
+				-- 	type = "gdb",
+				-- 	request = "attach",
+				-- 	program = function()
+				-- 		return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				-- 	end,
+				-- 	pid = function()
+				-- 		local name = vim.fn.input("Executable name (filter): ")
+				-- 		return require("dap.utils").pick_process({ filter = name })
+				-- 	end,
+				-- 	cwd = "${workspaceFolder}",
+				-- },
+				-- {
+				-- 	name = "Attach to gdbserver :1234",
+				-- 	type = "gdb",
+				-- 	request = "attach",
+				-- 	target = "localhost:1234",
+				-- 	program = function()
+				-- 		return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				-- 	end,
+				-- 	cwd = "${workspaceFolder}",
+				-- },
 			}
-			-- dap.adapters.gdb = {
-			--   type = 'executable',
-			--   command = 'gdb',
-			--   args = { '--interpreter=dap', '--eval-command', 'set print pretty on' },
-			-- }
 			-- dap.configurations.c = {
 			--   {
 			--     name = 'Launch file',
